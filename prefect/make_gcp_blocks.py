128147d6ce025285bbb085e41bbb6b712bf4e7a3
LadyTastingData <122860565+LadyTastingData@users.noreply.github.com>
2023-05-05 01:29:54 +0300

    Update make_gcp_blocks.py

--- prefect/make_gcp_blocks.py
+++ prefect/make_gcp_blocks.py
@@ -7,8 +7,8 @@ from pathlib import Path
 # IMPORTANT - do not store credentials in a publicly available repository!
 
 # Replace wity your own bucket_name and service_account_file
-bucket_name = "kaggle_data_lake_lithe-breaker-385610"
-service_account_file = Path("~/.google/credentials/lithe-breaker-385610-e22a767c96f3.json")
+bucket_name = "kaggle_data_lake_lithe-breaker-385610"  # insert your  GCS bucket name
+service_account_file = Path("~/.google/credentials/lithe-breaker-385610-e22a767c96f3.json")  # insert your  GCS credentials file
 
 credentials_block = GcpCredentials(
     service_account_file=service_account_file  # the json file with service account credentials
@@ -18,7 +18,7 @@ credentials_block.save("de-project-gcs-creds", overwrite=True)
 
 bucket_block = GcsBucket(
     gcp_credentials=GcpCredentials.load("de-project-gcs-creds"),
-    bucket=bucket_name  # insert your  GCS bucket name
+    bucket=bucket_name  
 )
 
 bucket_block.save("de-project-gcs", overwrite=True)
