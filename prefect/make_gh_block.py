c7bcdc7cfd7394922d9718b1afd22cafdfb903e4
Hande Topa <122860565+topahande@users.noreply.github.com>
2023-09-27 16:48:02 +0300

    Update make_gh_block.py

--- prefect/make_gh_block.py
+++ prefect/make_gh_block.py
@@ -3,7 +3,7 @@ from prefect.filesystems import GitHub
 # alternative to creating GitHub block in the UI
 
 gh_block = GitHub(
-    name="de-project-ghblock", repository="https://github.com/LadyTastingData/de-project"
+    name="de-project-ghblock", repository="https://github.com/topahande/de-project"
 )
 
 gh_block.save("de-project-ghblock", overwrite=True)
