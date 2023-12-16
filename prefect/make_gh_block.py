52208cd4521948ef75de0fc19737691e5e8eb02f
Hande Topa <122860565+topahande@users.noreply.github.com>
2023-12-16 14:27:14 +0200

    Update make_gh_block.py to reflect the repo name change.

--- prefect/make_gh_block.py
+++ prefect/make_gh_block.py
@@ -3,7 +3,7 @@ from prefect.filesystems import GitHub
 # alternative to creating GitHub block in the UI
 
 gh_block = GitHub(
-    name="de-project-ghblock", repository="https://github.com/topahande/de-project"
+    name="de-project-ghblock", repository="https://github.com/topahande/data-engineering-project"
 )
 
 gh_block.save("de-project-ghblock", overwrite=True)
